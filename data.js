a5045e38501ef019dbd88cd8779ccedddf9124dd
sarleykyts <133501183+sarleykyts@users.noreply.github.com>
2026-06-26 06:13:59 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,46 @@
+const playersData = [
+    {
+        id: 1,
+        name: "L. Messi",
+        overall: 105,
+        position: "FW",
+        country: "🇦🇷",
+        rarity: "epic",
+        stars: 5,
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png", // Replace with real transparent PNG
+        stats: { pace: 85, shooting: 96, passing: 98, dribbling: 99, defending: 40, physical: 65 }
+    },
+    {
+        id: 2,
+        name: "E. Haaland",
+        overall: 101,
+        position: "FW",
+        country: "🇳🇴",
+        rarity: "showtime",
+        stars: 5,
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/68.png",
+        stats: { pace: 90, shooting: 95, passing: 65, dribbling: 78, defending: 45, physical: 93 }
+    },
+    {
+        id: 3,
+        name: "M. Akanji",
+        overall: 96,
+        position: "DF",
+        country: "🇨🇭",
+        rarity: "big-time",
+        stars: 4,
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png",
+        stats: { pace: 82, shooting: 50, passing: 75, dribbling: 70, defending: 92, physical: 88 }
+    },
+    {
+        id: 4,
+        name: "K. De Bruyne",
+        overall: 100,
+        position: "MF",
+        country: "🇧🇪",
+        rarity: "epic",
+        stars: 5,
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+        stats: { pace: 76, shooting: 86, passing: 98, dribbling: 88, defending: 65, physical: 78 }
+    }
+];
